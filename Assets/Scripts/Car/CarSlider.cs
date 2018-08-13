2ce579ff73ad811907f4f8e55d5294855c49c2e0
Rebeca Calazans <rebecac.brito@gmail.com>
2018-08-13 15:42:16 -0300

    Correct Car Slider

--- Assets/Scripts/Car/CarSlider.cs
+++ Assets/Scripts/Car/CarSlider.cs
@@ -7,11 +7,13 @@ using UnityEngine.UI;
 public class CarSlider : MonoBehaviour {
   public Slider slider;
   public Image fill;
+  private CarGenerator carGenerator;
 
   public float duration = 100;
   float progress;
 
   void Start() {
+    carGenerator = GameObject.FindWithTag("CarGenerator").GetComponent<CarGenerator>();
     slider.value = 1;
   }
 
@@ -19,7 +21,10 @@ public class CarSlider : MonoBehaviour {
     slider.value = Mathf.MoveTowards(slider.value, 0, Time.deltaTime / duration);
     fill.color = new Color(1-slider.value, slider.value, 0);
 
-    if(slider.value == 0) Destroy(gameObject);
+    if (slider.value == 0) {
+      carGenerator.RemoveCarFromQueue(gameObject);
+      Destroy(gameObject);
+    }
   }
 
   void OnDisable() {
